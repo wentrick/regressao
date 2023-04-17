253f4154c0511551ea0b4c0cbb86dc004dc65dce
Wentrick <davi.w.feijo@gmail.com>
2023-04-17 09:50:15 -0300

    .

--- Exercicios/exercicios_sala_1.R
+++ Exercicios/exercicios_sala_1.R
@@ -54,6 +54,7 @@ sigma_quadrado = erro_quadrado/(n - 2)
 
 sigma = sqrt(sigma_quadrado)
 
+erro_padrao = sigma/sqrt(n)
 
 coeficiente_variacao = sigma/y_barra #desvio padrao divido pela media
 
@@ -78,23 +79,71 @@ coeficiente_variacao_beta_0 = s_beta_0/beta_0 #variancia maior pois incorpara a 
 
 
 
-#estimando intervalo de confianca para beta 1 com 95% de confianca 
-alfa = 0.95
+#estimando intervalo de confianca para beta 1 com 95% de confianca (os sinais estao "trocados pq nao usei o lower.tail = FALSE")
+alfa = 0.1
 
-ic_beta_1  = c(beta_1+qt((1-0.95)/2, n-2)*s_beta_1,beta_1-qt((1-0.95)/2, n-2)*s_beta_1)
+ic_beta_1  = c(beta_1-qt(alfa/2, n-2,lower.tail = FALSE)*s_beta_1,beta_1+qt(alfa/2, n-2,lower.tail = FALSE)*s_beta_1)
 
 #estimando intervalo de confianca para beta 0 com 95% de confianca 
+alfa = 0.1
+
+ic_beta_0  = c(beta_0-qt(alfa/2, n-2,lower.tail = FALSE)*s_beta_0,beta_0+qt((alfa/2), n-2,lower.tail = FALSE)*s_beta_0)
+
+#teste de hipotese sobre beta 1 (teste de ausencia de regressao)
+
+#Ho: beta_1 = 0
+#H0: beta_1 > 0 (ou != 0)
 alfa = 0.95
+teste_stat = beta_1/s_beta_1
 
-ic_beta_0  = c(beta_0+qt((1-0.95)/2, n-2)*s_beta_0,beta_0-qt((1-0.95)/2, n-2)*s_beta_0)
+rc = qt(alfa,n-2) #regiao critica
 
-#teste de hipotese sobre beta 1
+pvalor = pt(teste_stat,n-2, lower.tail = FALSE) #pvalor
 
+pvalor < alfa #pvalor menos que o alfa de 5% logo rejeitamos H0
 
-
-
-
-
-
+#logo existe uma regressao ou seja beta 1 contribui para a relacao da massa sem gordura e o metabolismo
 
 
+#teste de hipoteses sobre b0
+
+#
+#
+
+
+
+#teste de hipotese e intervalo de confianca sobre sigma quadrado
+
+gl = n-2
+alfa = 0.1
+
+qchisq(alfa/2,gl)
+
+qchisq(alfa/2,gl,lower.tail = FALSE)
+
+ic_sigma_quadrado = c(((n-2)*sigma_quadrado/qchisq(alfa/2,gl,lower.tail = FALSE)),((n-2)*sigma_quadrado/qchisq(alfa/2,gl)))
+
+#ou podemos usar os residuos - verificar nos slides ta dando errado
+
+#ic_sigma_quadrado = c((erro^2/qchisq(alfa/2,gl,lower.tail = FALSE)),(erro^2/qchisq(alfa/2,gl)))
+
+#estimativa intervalar da média de Y dado um x
+
+x_escolhido = 50
+
+#calculo usando o erro padrao (verificar se esta correto)
+t_value <- qt(0.975, n - 2,lower.tail = TRUE) # Valor crítico da distribuição t-Student para o nível de confiança de 95% e n-2 graus de liberdade
+y_new_hat <- beta_0 + beta_1*x_escolhido # Estimativa pontual da média de Y para o valor de X escolhido
+ic_lower <- y_new_hat - t_value*erro_padrao # Limite inferior do intervalo de confiança
+ic_upper <- y_new_hat + t_value*erro_padrao # Limite superior do intervalo de confiança
+
+cat("Intervalo de confiança para a média de Y quando X =", x_escolhido, ": [", ic_lower, ",", ic_upper, "]")
+
+#calculando alternativo (feito em aula)
+
+t_value <- qt(0.975, n - 2,lower.tail = TRUE) # Valor crítico da distribuição t-Student para o nível de confiança de 95% e n-2 graus de liberdade
+y_new_hat <- beta_0 + beta_1*x_escolhido # Estimativa pontual da média de Y para o valor de X escolhido
+ic_lower <- y_new_hat - t_value*sigma*sqrt((1/n)+(x_escolhido- x_barra)^2)/(x_quadrado - n*x_barra^2)) # Limite inferior do intervalo de confiança
+ic_upper <- y_new_hat + t_value*sigma*sqrt(((1/n)+((x_escolhido- x_barra)^2)/(x_quadrado - n*x_barra^2))) # Limite superior do intervalo de confiança
+#arrumar os parenteses da equacao
+cat("Intervalo de confiança para a média de Y quando X =", x_escolhido, ": [", ic_lower, ",", ic_upper, "]")
